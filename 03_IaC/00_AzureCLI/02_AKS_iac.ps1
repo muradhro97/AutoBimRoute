58b9e10a5136573d0c2abbd2db124dc7d7f3a5f6
General <muradhro97@gmail.com>
2023-11-20 03:30:51 +0200

    20/11 update 02_AKS_iac.ps1

--- 03_IaC/00_AzureCLI/02_AKS_iac.ps1
+++ 03_IaC/00_AzureCLI/02_AKS_iac.ps1
@@ -42,19 +42,8 @@ az aks nodepool add --resource-group $resourceGroupName --cluster-name $AKSClust
 Write-Output "About to create ACR: $acrName"
 az acr create --resource-group $resourceGroupName --name $acrName --sku Basic --admin-enabled true
 
-Write-Output "Updating AKS cluster with ACR"
-az aks update -n $AKSClusterName -g $resourceGroupName --attach-acr $acrName
-
-Write-Output "Creating teams-recording-bot namespace and bot secret that holds BOT_ID, BOT_SECRET, BOT_NAME, Cognitive Service Key and Middleware End Point"
-Write-Output "Botname is: $env:botName and Persistance end point is: $env:persistenceEndPoint, Application Insights Key is: $appInsightsKey"
-Write-Output "kubectl create secret generic bot-application-secrets --namespace teams-recording-bot --from-literal=applicationId=$env:BOT_ID
- --from-literal=applicationSecret=$env:BOT_SECRET
- --from-literal=botName=$env:botName
- --from-literal=azureCognitiveKey=$env:azureCognitiveKey
- --from-literal=persistenceEndPoint=$env:persistenceEndPoint
- --from-literal=azureCognitiveRegion=$azureLocation
- --from-literal=appInsightsKey=$appInsightsKey
- --from-literal=serviceDnsName=$botSubDomain"
+#Write-Output "Updating AKS cluster with ACR"
+#az aks update -n $AKSClusterName -g $resourceGroupName --attach-acr $acrName
 
 # Move the Public IP address to MC_ resource group. 
 # This is needed in order for the load balancer to get assigned with the Public IP, otherwise you might end up in a "pending" state.
@@ -70,9 +59,9 @@ az aks get-credentials --resource-group $resourceGroupName --name $AKSClusterNam
 # on first run this will give errors, but when running it again it will restore things to initial state.
 
 # delete namespace to clean things up.
-kubectl delete ns teams-recording-bot
+#kubectl delete ns teams-recording-bot
 # make sure the secret is updated - so delete it if there
-kubectl delete secrets bot-application-secrets --namespace teams-recording-bot
+#kubectl delete secrets bot-application-secrets --namespace teams-recording-bot
 
 # Create a Public Ip on the MC_RESOURCEGROUP_AKSCLUSTERNAME_AZUREREGION Resource group
 #done
@@ -85,26 +74,18 @@ kubectl delete secrets bot-application-secrets --namespace teams-recording-bot
 
 # Create namespace
 #done
-Write-Output "About to create teams-recording-bot namespace"
-kubectl create ns teams-recording-bot
+#Write-Output "About to create teams-recording-bot namespace"
+#kubectl create ns teams-recording-bot
 
 # Upload certs
-Write-Host "Uploading certificate to new secret" -ForegroundColor Yellow
-kubectl create secret tls tls-secret --cert=C:\Users\vasalis\Desktop\RecordingBotCerts\tls.crt --key=C:\Users\vasalis\Desktop\RecordingBotCerts\tls.key --namespace teams-recording-bot
+#Write-Host "Uploading certificate to new secret" -ForegroundColor Yellow
+#kubectl create secret tls tls-secret --cert=C:\Users\vasalis\Desktop\RecordingBotCerts\tls.crt --key=C:\Users\vasalis\Desktop\RecordingBotCerts\tls.key --namespace teams-recording-bot
 
 
 # Setup AKS namespace for teams-recording-bot
 #done
 Write-Output "Creating teams-recording-bot namespace and bot secret that holds BOT_ID, BOT_SECRET, BOT_NAME, Cognitive Service Key and Middleware End Point"
 Write-Output "Botname is: $env:botName and Persistance end point is: $env:persistenceEndPoint, Application Insights Key is: $appInsightsKey"
-Write-Output "kubectl create secret generic bot-application-secrets --namespace teams-recording-bot --from-literal=applicationId=$env:BOT_ID
- --from-literal=applicationSecret=$env:BOT_SECRET
- --from-literal=botName=$env:botName
- --from-literal=azureCognitiveKey=$env:azureCognitiveKey
- --from-literal=persistenceEndPoint=$env:persistenceEndPoint
- --from-literal=azureCognitiveRegion=$azureLocation
- --from-literal=appInsightsKey=$appInsightsKey
- --from-literal=serviceDnsName=$botSubDomain"
 
 # done
 kubectl create secret generic bot-application-secrets --namespace teams-recording-bot --from-literal=applicationId=$env:BOT_ID `
